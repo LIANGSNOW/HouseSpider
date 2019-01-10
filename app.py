cc5ee461b9b69ea8565ec4b00bffa5072f4e480f
qw <e0013379@e.nus.edu>
2019-01-10 21:22:58 +0800

    update for getting much more info

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import pandas as pd
+from multiprocessing import Pool
 
 from fake_useragent import UserAgent
 ua = UserAgent()
@@ -18,6 +20,10 @@ def main():
     user_in_nub = input('输入生成页数：')
     for i in generate_allurl(user_in_nub):
         print(i)
+        urls = get_allurl(i)
+        for url in urls:
+            open_url(url)
+
 
 def get_allurl(generate_allurl):
     get_url = requests.get(generate_allurl,'lxml',headers = headers1)
@@ -38,13 +44,52 @@ def open_url(re_get):
         info['标题'] = soup.select('.main')[0].text
         info['总价'] = soup.select('.total')[0].text + '万'
         info['每平方售价'] = soup.select('.unitPriceValue')[0].text
-        print( info)
+        info['建造时间'] = soup.select('.subInfo')[2].text
+        info['小区名称'] = soup.select('.info')[0].text
+        info['所在区域'] = soup.select('.info a')[0].text + ':' + soup.select('.info a')[1].text
+        #查找房屋户型
+        ul =  soup.find_all('ul')[5]
+        info['房屋户型'] = ul.find_all('li')[0].text
+        # info['小区均价'] = soup.find_all('',{"class":'xiaoquCard'})
+
+        #获取所有房间信息
+        rooms = soup.select('.row')
+        for room in rooms:
+            if len(room.select('.col'))!=0:
+                info[room.select('.col')[0].text] = room.select('.col')[1].text
+
+        info['链家编号'] = str(re_get)[33:].rsplit('.html')[0]
+        for i in soup.select('.base li'):
+            i = str(i)
+            if '</span>' in i or len(i) > 0:
+                key, value = (i.split('</span>'))
+                info[key[24:]] = value.rsplit('</li>')[0]
+
+
+        print(info)
+        # pandas_to_xlsx(info)
+        return info
+
+
+def pandas_to_xlsx(info):               #储存到xlsx
+    pd_look = pd.DataFrame(info)
+    pd_look.to_excel('链家二手房.xlsx',sheet_name='链家二手房')
+
 
 if __name__ == '__main__':
-    # main()
-    urls = get_allurl('https://sh.lianjia.com/ershoufang/pg1')
-    for url in urls:
-        open_url(url)
+    """
+    multi thread
+    """
+    # pool = Pool()
+    # pool.map(main,[])
+
+    main()
+
+
+
+    # urls = get_allurl('https://sh.lianjia.com/ershoufang/pg1')
+    # for url in urls:
+    #     open_url(urls[1])
     # get_url = requests.get('https://sh.lianjia.com/ershoufang/pg1', 'lxml', headers=headers1)
     # print(get_url)
     # re_set = re.compile('<li.*?class="clear LOGCLICKDAT">.*?<a.*?class="noresultRecommend img.*?".*?href="(.*?)"')
